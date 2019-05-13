bdda66b052017492b10c28be821adb84f98a9913
Michael Hathaway <michael@hathaways.me.uk>
2019-05-14 00:17:39 +0100

    Added missing semi-colons

--- browser.ts
+++ browser.ts
@@ -3,13 +3,13 @@ import { MoveType, Outcome, Game } from "./rpsls";
 const inputbox = document.getElementById("input-box") as HTMLInputElement;
 document.addEventListener("keyup", e => {
     if (e.keyCode === 13) {
-        playRound(<MoveType>inputbox.value)
-        inputbox.value = ""
+        playRound(<MoveType>inputbox.value);
+        inputbox.value = "";
     }
 })
 document.getElementById("submit").addEventListener( "click", e => {
-    playRound(<MoveType>inputbox.value)
-    inputbox.value = ""
+    playRound(<MoveType>inputbox.value);
+    inputbox.value = "";
 })
 
 const game = new Game(5);
@@ -26,25 +26,25 @@ function playRound(playerMove: MoveType){
             }
         }
         const r = game.runRound(MoveType[move], cpuChoice);
-        displayLine(r.toString())
+        displayLine(r.toString());
     } else {
-        displayLine("You fumble around for too long. You are eaten by a grue.")
+        displayLine("You fumble around for too long. You are eaten by a grue.");
     }
     const gameStatus = game.isOver()
     if (gameStatus.status) {
-        displayLine("Well, I'm glad that's over.")
+        displayLine("Well, I'm glad that's over.");
         if (gameStatus.winner === Outcome.P1) {
             displayLine("Drat! You won!");
         } else {
-            displayLine("Muhahaha! You failed to defeat me!")
-            displayLine("Your enemy vanquises you.")
+            displayLine("Muhahaha! You failed to defeat me!");
+            displayLine("Your enemy vanquises you.");
         }
     }
 }
 function displayLine(line: string) {
     const outBox = document.getElementById("printout");
     const template = document.getElementById("event-line") as HTMLTemplateElement;
-    let newLine = document.importNode(template.content, true)
+    let newLine = document.importNode(template.content, true);
     newLine.querySelectorAll("p")[0].textContent = line;
     outBox.appendChild(newLine);
     outBox.scrollTop = outBox.scrollHeight;
--- main.ts
+++ main.ts
@@ -4,4 +4,4 @@ import { MoveType, Round } from "./rpsls"
 let round = new Round(MoveType.ROCK, MoveType.PAPER);
 console.log(round.toString());
 let round2 = new Round(MoveType.ROCK, MoveType.ROCK);
-console.log(round2.toString())
+console.log(round2.toString());
