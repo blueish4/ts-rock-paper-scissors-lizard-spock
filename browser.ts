4c6a7bcfbb488fbf6d0f4124fed0ad3f9ce45d49
blueish <blueish4@users.noreply.github.com>
2019-05-14 00:06:13 +0100

    Randomise cpu choice

--- browser.ts
+++ browser.ts
@@ -17,7 +17,15 @@ function playRound(playerMove: MoveType){
     displayLine("> " + playerMove);
     const move = playerMove.toUpperCase();
     if (MoveType[move] != null){
-        const r = game.runRound(MoveType[move], MoveType.ROCK);
+        
+        let count = 0;
+        let cpuChoice = MoveType.SPOCK;
+        for (const prop in MoveType) {
+            if (Math.random() < 1/++count){
+                cpuChoice = <MoveType>MoveType[prop];
+            }
+        }
+        const r = game.runRound(MoveType[move], cpuChoice);
         displayLine(r.toString())
     } else {
         displayLine("You fumble around for too long. You are eaten by a grue.")
